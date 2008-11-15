fdb328c479c06649970f9b0127e5a7164f4aba0b
fccoelho <fccoelho@localhost>
2008-11-15 16:42:14 +0000

    added tests to like module

---
+++ trunk/Bayes/tests/test_general_like.py
@@ -0,0 +1,46 @@
+from nose import SkipTest
+
+class TestCategor:
+    def test_categor(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestNormal:
+    def test_normal(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestLognormal:
+    def test_lognormal(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestPoisson:
+    def test_poisson(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestNegbin:
+    def test_negbin(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestBinomial:
+    def test_binomial(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestWeibull:
+    def test_weibull(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestBernoulli:
+    def test_bernoulli(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestGamma:
+    def test_gamma(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestBeta:
+    def test_beta(self):
+        raise SkipTest # TODO: implement your test here
+
+class TestSimple:
+    def test_simple(self):
+        raise SkipTest # TODO: implement your test here
+
