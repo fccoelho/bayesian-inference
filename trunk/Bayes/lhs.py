fcfd594ec8ba7a49f1a10a1b2b85887a117ebb7b
fccoelho <fccoelho@localhost>
2008-11-18 15:39:29 +0000

    Added Melding and lhs module these will be part of a basic inference modules to be collected in the Bayes package

---
+++ trunk/Bayes/lhs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from pylab import plot, figure,hist,show, savefig, legend
+import scipy.stats as stats
+import numpy
+
+def lhs(dist, parms, n=100):
+    '''
+    Latin Hypercube sampling of any distrbution.
+    dist is is a scipy.stats random number generator 
+    such as stats.norm, stats.beta, etc
+    parms is a tuble with the parameters needed for 
+    the specified distribution.
+    '''
+    perc = numpy.arange(0,1.,1./n)
+    smp = [stats.uniform(i,1./n).rvs()[0] for i in perc]
+    v = dist(*parms).ppf(smp)
+    return v
+            
+if __name__=='__main__':
+    dist = stats.norm
+    #dist = stats.beta
+    pars = (0,1)
+    #pars = (1,5) #beta
+    c=lhs(dist, pars,20)
+    hist(c,normed=1, label='LHS sample')
+    n = dist(*pars).rvs(size=20)
+    hist(n.ravel(),facecolor='r',alpha =0.3,normed=1, label='Regular sample')
+    plot(numpy.arange(min(min(c),min(n)),max(max(c),max(n)),.1),dist(*pars).pdf(numpy.arange(min(min(c),min(n)),max(max(c),max(n)),.1)),label='PDF')
+    legend()#['LHS sample','regular sample', 'PDF'])
+    #savefig('lhs.png',dpi=400)
+    show()
+    
