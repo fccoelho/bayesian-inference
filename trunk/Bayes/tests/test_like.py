32be0fcd1cbc2ad029ca7ade9133a8f1f869bf84
fccoelho <fccoelho@localhost>
2008-11-20 11:18:55 +0000

    added more tests

--- trunk/Bayes/tests/test_like.py
+++ trunk/Bayes/tests/test_like.py
@@ -1,6 +1,8 @@
 import unittest
 from numpy import arange
+from numpy.random import lognormal
 from scipy import stats
+from Bayes.like import Normal, Lognormal, Poisson
 
 class TestCategor(unittest.TestCase):
     def test_categor(self):
@@ -8,21 +10,35 @@ class TestCategor(unittest.TestCase):
 
 class TestNormal(unittest.TestCase):
     def test_normal(self):
-        data = stats.norm.rvs(loc=0, scale=1, size=100)
+        data = stats.norm.rvs(loc=0, scale=1, size=1000)
         mle = [Normal(data, i, 1) for i in arange(-2, 2, .1)]
         mle = arange(-2, 2, .1)[mle.index(max(mle))]
-        assert 0-mle <0.000001
+        print "Normal MLE error: ", (0-mle)
+        assert abs(0-mle) <0.000001
 class TestLognormal(unittest.TestCase):
     def test_lognormal(self):
-        assert False # TODO: implement your test here
+        data = lognormal.rvs(loc=5, scale=1, size=1000)
+        mle = [Poisson(data, i) for i in arange(0, 10, .1)]
+        mle = arange(0, 10, .1)[mle.index(max(mle))]
+        print "LogNormal MLE error: ", (5-mle)
+        assert abs(5-mle) <0.000001
 
 class TestPoisson(unittest.TestCase):
     def test_poisson(self):
-        assert False # TODO: implement your test here
+        data = stats.poisson.rvs(5, size=1000)
+        ll = [Poisson(data, i) for i in arange(0, 10, .1)]
+        mle = arange(0, 10, .1)[ll.index(max(ll))]
+        print "Poisson MLE error: ", (5-mle)
+        assert abs(5-mle) <0.000001
 
 class TestNegbin(unittest.TestCase):
     def test_negbin(self):
-        assert False # TODO: implement your test here
+        #TODO Unfinished! 
+        data = stats.nbinom .rvs(n=10, pr=.5, size=1000)
+        mle = [Negbin(data, 10, i) for i in arange(0, 1, .01)]
+        mle = arange(0, 1, .01)[mle.index(max(mle))]
+        print "Negative Binomial MLE error: ", (.5-mle)
+        assert abs(.5-mle) <0.000001
 
 class TestBinomial(unittest.TestCase):
     def test_binomial(self):
