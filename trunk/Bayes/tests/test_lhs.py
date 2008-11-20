6e8d195d705822d8dd38286bc77ad26bb07e66b7
fccoelho <fccoelho@localhost>
2008-11-20 10:46:00 +0000

    added more tests

---
+++ trunk/Bayes/tests/test_lhs.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestLhs(unittest.TestCase):
+    def test_lhs(self):
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
