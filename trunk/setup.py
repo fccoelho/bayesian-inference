d4d01aee0b12e72a4c963bbe26e57b94c2589e3c
fccoelho <fccoelho@localhost>
2008-11-21 19:36:25 +0000

    started the setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,1 +1,15 @@
- 
+# -*- coding:utf8 -*-
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+
+setup(name='BIP', 
+        version  = '0.1a',
+        author = 'Flávio Codeço Coelho', 
+        author_email = 'fccoelho@gmail.com', 
+        url = '', 
+        description = '', 
+        test_suite = 'nose.collector', 
+        license = 'GPLv2', 
+        
+      )
