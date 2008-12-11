f1707cca4d678281f1cb01d17ba943108ce92afc
fccoelho <fccoelho@localhost>
2008-12-11 16:16:27 +0000

    lhs sampling function modified to accept sample size as a shape tuple or an integer. some improvements to Meld class

--- trunk/BIP/Bayes/tests/test_lhs.py
+++ trunk/BIP/Bayes/tests/test_lhs.py
@@ -1,8 +1,11 @@
 import unittest
+from BIP.Bayes import lhs
+from scipy import stats
 
 class TestLhs(unittest.TestCase):
     def test_lhs(self):
-        assert False # TODO: implement your test here
+        s = lhs.lhs(stats.norm,(0,1),size=(10,100))
+        assert s.shape == (10,100)
 
 if __name__ == '__main__':
     unittest.main()
