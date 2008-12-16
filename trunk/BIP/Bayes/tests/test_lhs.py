bb992896d7a775034c93e7ca116ae3276d834fd4
fccoelho <fccoelho@localhost>
2008-12-16 17:00:27 +0000

    Implemented lhsFromDensity on lhs module. added tests for it but they are still empty

--- trunk/BIP/Bayes/tests/test_lhs.py
+++ trunk/BIP/Bayes/tests/test_lhs.py
@@ -7,5 +7,13 @@ class TestLhs(unittest.TestCase):
         s = lhs.lhs(stats.norm,(0,1),size=(10,100))
         assert s.shape == (10,100)
 
+class TestLhsFromSample(unittest.TestCase):
+    def test_lhs_from_sample(self):
+        assert False # TODO: implement your test here
+
+class TestLhsFromDensity(unittest.TestCase):
+    def test_lhs_from_density(self):
+        assert False # TODO: implement your test here
+
 if __name__ == '__main__':
     unittest.main()
