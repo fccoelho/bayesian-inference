c934c2a8ddfff623553e1644e2778411a9b743de
fccoelho <fccoelho@localhost>
2009-03-17 10:10:45 +0000

    removed import of visualization module from Melding.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='BIP', 
-        version  = '0.3.5',
+        version  = '0.3.6',
         author = 'Flavio Codeco Coelho', 
         author_email = 'fccoelho@gmail.com', 
         url = 'http://code.google.com/p/bayesian-inference/',
