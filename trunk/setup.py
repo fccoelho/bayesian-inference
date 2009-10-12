a87a406d4442c88b0fefaa77da40329841a951fd
fccoelho <fccoelho@localhost.localdomain>
2009-10-12 13:39:21 +0100

    removed 2.6 only syntax to make it compatible with python 2.5

--- trunk/setup.py
+++ trunk/setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='BIP', 
-        version  = '0.3.17',
+        version  = '0.3.18',
         author = 'Flavio Codeco Coelho', 
         author_email = 'fccoelho@gmail.com', 
         url = 'http://code.google.com/p/bayesian-inference/',
