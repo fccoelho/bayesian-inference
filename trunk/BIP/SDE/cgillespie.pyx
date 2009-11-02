4be8652713643c8bc30fe4cd0b1ce71531468fed
Flavio Codeco Coelho <fccoelho@gmail.com>
2009-11-02 12:42:00 +0000

    improved cgillespie performance

--- trunk/BIP/SDE/cgillespie.pyx
+++ trunk/BIP/SDE/cgillespie.pyx
@@ -3,15 +3,21 @@ from numpy.random import uniform, multinomial, exponential
 import numpy as np
 cimport numpy as np
 import time
+from random import random
 
 DTYPE = np.double
 ctypedef np.double_t DTYPE_t
 ctypedef np.int_t INT_t
 
-cdef class Model:
-    cdef object vn,rates,inits,pv
-    cdef np.ndarray tm,res,time,series
-    cdef np.int pvl,nvars,steps
+cdef extern from "math.h":
+    double log(double)
+cdef double clog(double x):
+    return log(x*x)
+
+cdef class Model(object):
+    cdef object vn,pv, inits
+    cdef np.ndarray tm,res,time,series, rates
+    cdef unsigned int pvl,nvars,steps
     cdef object ts
     def __init__(self,vnames,rates,inits, tmat,propensity):
         '''
@@ -22,8 +28,8 @@ cdef class Model:
             lambda r,ini: some function of rates ans inits.
         '''
         self.vn = vnames
-        self.rates = rates
-        self.inits = inits
+        self.rates = np.array(rates)
+        self.inits = tuple(inits)
         self.tm = tmat
         self.pv = propensity#[compile(eq,'errmsg','eval') for eq in propensity]
         self.pvl = len(self.pv) #length of propensity vector
@@ -32,37 +38,37 @@ cdef class Model:
         self.series = np.zeros(1)
         self.steps = 0
 
-    def run(self, method='SSA', int tmax=10, int reps=1):
-        cdef np.ndarray[DTYPE_t,ndim=3] res = np.zeros((tmax,self.nvars,reps),dtype=float)
-        tvec = np.arange(tmax)
+    cpdef run(self, method='SSA', int tmax=10, int reps=1):
+        cdef np.ndarray[np.int_t,ndim=3] res = np.zeros((tmax,self.nvars,reps),dtype=np.int)
+        cdef np.ndarray[np.int_t] tvec = np.arange(tmax,dtype=np.int)
         self.res = res
         cdef int i, steps
         if method =='SSA':
             for i from 0 <= i<reps:
                 steps = self.GSSA(tmax,i)
-            print steps,' steps'
+            #print steps,' steps'
         elif method == 'SSAct':
             pass
         self.time=tvec
-        self.series=self.res
+       # self.series=self.res
         self.steps=steps
     
     def getStats(self):
-        return self.time,self.series,self.steps
+        return self.time,self.res,self.steps
     
-    cpdef int GSSA(self, int tmax=50,int round=0) except *:
+    cdef int GSSA(self, unsigned int tmax=50, unsigned int round=0) except *:
         '''
         Gillespie Direct algorithm
         '''
-        ini = self.inits
-        r = self.rates
+        cdef np.ndarray[np.int_t] ini = np.array(self.inits)
+        cdef np.ndarray[np.double_t] r = self.rates
         pvi = self.pv
-        cdef np.int l,steps,i,tim
+        cdef int l,steps,i,tim
         cdef double tc, tau, a0
         #cdef np.ndarray[INT_t] tvec
-        cdef np.ndarray[np.double_t] pv
+        cdef np.ndarray[np.double_t] pv 
         l=self.pvl
-        pv = np.zeros(l)
+        pv = np.zeros(l,dtype=np.double)
         tm = self.tm
         #tvec = np.arange(tmax,dtype=int)
         tc = 0
@@ -76,7 +82,7 @@ cdef class Model:
                 #pv = abs(array([eq() for eq in pvi]))# #propensity vector
                 a0 = a_sum(pv,l) #sum of all transition probabilities
                 #print ini#,tim, pv, a0
-                tau = (-1/a0)*np.log(np.random.random())
+                tau = (-1/a0)*clog(random())
                 if pv.any():
                     event = np.random.multinomial(1,(pv/a0)) # event which will happen on this iteration
                     ini += tm[:,event.nonzero()[0][0]]
@@ -96,12 +102,12 @@ cdef class Model:
         """
         pass
         
-def l1(np.ndarray r,np.ndarray ini):
+cpdef double l1(np.ndarray r,np.ndarray ini):
     return r[0]*ini[0]*ini[1]
-def l2(np.ndarray r,np.ndarray ini):
+cpdef double l2(np.ndarray r,np.ndarray ini):
     return r[1]*ini[1]
 
-def main():
+cpdef main():
     vars = ['s','i','r']
     cdef np.ndarray[np.int_t] ini= np.array([500,1,0],dtype = np.int)
     cdef np.ndarray[np.float64_t] rates = np.array([.001,.1],dtype=np.float64)
@@ -114,13 +120,13 @@ def main():
     return M.getStats()
 
     
-cdef double a_sum(np.ndarray a, int len):
+cdef double a_sum(np.ndarray a, unsigned int len):
     cdef double s
     cdef int i
-    s=0
-    for i from 0 <=i <len:
-        s+=a[i]
+    s = 0
+    for i from 0 <= i < len:
+        s += a[i]
     return s
-        
+
 
 
