ea725b4f01d788cb0ac0e9d1c085dab7d35629a9
Flavio Codeco Coelho <fccoelho@gmail.com>
2009-12-09 11:25:36 +0000

    Added MCMC module stub

---
+++ trunk/BIP/Bayes/Samplers/MCMC.py
@@ -0,0 +1,17 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+"""
+Module implementing MCMC samplers 
+
+"""
+
+__author__="fccoelho"
+__date__ ="$09/12/2009 10:44:11$"
+__docformat__ = "restructuredtext en"
+
+class Metropolis(object):
+    def __init__(self, proposal_dist,):
+        pass
+
+if __name__ == "__main__":
+    pass
