b8d4f746c2a58a4695a750e086e0765d310ba062
Flavio Codeco Coelho <fccoelho@gmail.com>
2010-02-26 11:08:04 +0000

    added date support for x axis on violin and cumulative predicted plots

--- trunk/BIP/Bayes/PlotMeld.py
+++ trunk/BIP/Bayes/PlotMeld.py
@@ -14,7 +14,9 @@ import glob
 import datetime
 from numpy import *
 import matplotlib.pyplot as P
+from matplotlib.dates import date2num
 from scipy.stats import gaussian_kde
+
 
 
 def plot_series(tim,obs, series, names=[],title='series'):
@@ -74,14 +76,22 @@ def pred_new_cases(obs,series,weeks,names=[],ws=7):
         names = series[0].dtype.names
     ax = fig.add_subplot(111)
     c = cycle(['b','g','r','c','m','y','k'])
+    if 'time' in obs: #setting the xlabel 
+        x = date2num([obs['time'][ws*i] for i in range(1, weeks)])
+        W = 3
+        ax.xaxis_date()
+    else:
+        x = range(weeks)
     for n in names:
         if n in obs:
             co = c.next()
-            ax.boxplot([sum(s[n],axis=1) for s in series] ,positions = range(weeks),notch=1,vert=1)
-            ax.plot(range(weeks),[mean(sum(s[n],axis=1)) for s in series],'%s^'%co, label="Mean pred. %s"%n)
-            ax.plot(range(weeks-1),[sum(obs[n][(w+1)*ws:(w+1)*ws+ws]) for w in range(weeks-1)],'%s-o'%co, label="obs. Prev")
+            ax.plot([date2num(obs['time'][ws])]+x.tolist(), [mean(sum(s[n],axis=1)) for s in series],'%s^'%co, label="Mean pred. %s"%n)
+            ax.plot(x,[sum(obs[n][(w+1)*ws:(w+1)*ws+ws]) for w in range(weeks-1)],'%s-o'%co, label="obs. Prev")
+            ax.boxplot([sum(s[n],axis=1) for s in series] ,positions = x, widths=W,notch=1,vert=1)
     P.xlabel('windows')
     ax.legend(loc=0)
+    if 'time' in obs: 
+        fig.autofmt_xdate()
 
 def plot_series2(tim,obs,series,names=[],title='Simulated vs Observed series',wl=7,lag=False):
     ser2={}
@@ -91,7 +101,6 @@ def plot_series2(tim,obs,series,names=[],title='Simulated vs Observed series',wl
     tim = tim[:ls]
     #print type (series)#.I.shape
     fig =P.figure()
-    
     if not names:
         names = series[0].dtype.names
     c = cycle(['b','g','r','c','m','y','k'])
@@ -101,14 +110,23 @@ def plot_series2(tim,obs,series,names=[],title='Simulated vs Observed series',wl
         lag = int(lag)*wl
     for i, n in enumerate(names):
         ax = fig.add_subplot(len(names), 1, i+1)
+        if isinstance(tim[0], datetime.date):
+            ax.xaxis_date()
         co = c.next()
         if n in obs:
             ax.plot(tim,obs[n][:len(tim)],'%s+'%co, label="Observed %s"%n)
             #print len(tim),  ls
         ax.plot(array(tim)+lag,median(ser2[n],axis=0),'k-', label=n)
-        lower = [stats.scoreatpercentile(t,2) for t in ser2[n].T]
-        upper =[stats.scoreatpercentile(t,98) for t in ser2[n].T]
-        ax.fill_between(array(tim)+lag,lower,upper,facecolor=co,alpha=0.6)
+        lower = [stats.scoreatpercentile(t,2.5) for t in ser2[n].T]
+        upper =[stats.scoreatpercentile(t,97.5) for t in ser2[n].T]
+        dif = (array(upper)-array(lower))
+        dif  = dif/max(dif)*10
+        pe, va = peakdet(dif, 1)
+        xp = [0]+ pe[:, 0].tolist()+[len(lower)-1]
+        lower_i = interp(range(len(lower)), xp, array(lower)[xp]) # valley-to-valley interpolated band
+        upper_i = interp(range(len(upper)), xp, array(upper)[xp])#peak-to-peak interpolated band
+        ax.fill_between(array(tim)+lag,lower_i,upper_i,facecolor=co,alpha=0.1)
+        #ax.fill_between(array(tim)+lag,lower,upper,facecolor='k',alpha=0.1)
         if i < (len(names)-1):ax.xaxis.set_ticklabels([])
         ax.legend()
         if i == 0:
@@ -116,6 +134,8 @@ def plot_series2(tim,obs,series,names=[],title='Simulated vs Observed series',wl
     #ax.xaxis.set_visible(True)
     #P.title(title)
     P.xlabel('days')
+    if isinstance(tim[0], datetime.date):
+        fig.autofmt_xdate()
     
 
 def plot_par_series(tim,ptlist):
@@ -128,7 +148,7 @@ def plot_par_series(tim,ptlist):
         P.boxplot([s[n] for s in ptlist],notch=1,positions=tim,vert=1)
         #P.errorbar(tim,[median(t[n]) for t in ptlist],yerr=[std(t[n]) for t in ptlist],label=n)
         P.ylabel(n)
-    P.xlabel('Weeks')
+    P.xlabel('Windows')
 
 def plot_par_violin(tim,ptlist, priors={}, bp=True):
     fig = P.figure()
@@ -138,15 +158,21 @@ def plot_par_violin(tim,ptlist, priors={}, bp=True):
     if len(ptlist[0].dtype.names) == 3:
         r = 1; c = 3
     if priors:
-        if len(tim)==1:
-            tim  = [-1, 0]
+        if isinstance(tim[0], datetime.date):
+            pdate = tim[0]-datetime.timedelta(1) if len(tim)==1 else tim[0]-(tim[1]-tim[0])
+            tim =[pdate]+tim.tolist()
         else:
-            tim = [tim[0]-(tim[1]-tim[0])]+tim
+            if len(tim)==1:
+                tim  = [-1, 0]
+            else:
+                tim = [tim[0]-(tim[1]-tim[0])]+tim
     for i,n in enumerate(ptlist[0].dtype.names):
         ax = fig.add_subplot(r,c,i+1)
         violin_plot(ax,[priors[n]]+[s[n] for s in ptlist],tim,bp, True)
         P.ylabel(n)
-    P.xlabel('Weeks')
+    P.xlabel('Windows')
+    if isinstance(tim[0], datetime.date):
+        fig.autofmt_xdate()
 
 def violin_plot(ax,data,positions,bp=False, prior = False):
     '''
@@ -159,9 +185,13 @@ def violin_plot(ax,data,positions,bp=False, prior = False):
         - `bp`: Whether to plot the boxplot on top.
         - `prior`: whether the first element of data is a Prior distribution.
     '''
+    if isinstance(positions[0], datetime.date):
+        ax.xaxis_date()
+        positions = date2num(positions)
     dist = max(positions)-min(positions)
-    w = min(0.15*max(dist,1.0),0.5)
+    w = 3#min(0.5*max(dist,1.0),0.5)
     i = 0
+    
     for d,p  in zip(data, positions):
         if prior and i == 0:
             color = 'g'
@@ -177,7 +207,74 @@ def violin_plot(ax,data,positions,bp=False, prior = False):
         ax.fill_betweenx(x,p,-v+p,facecolor=color,alpha=0.3)
         i+=1
     if bp:
-        ax.boxplot(data,notch=1,positions=positions,vert=1)
+        ax.boxplot(data,notch=1,positions=positions,widths=w, vert=1)
+
+def peakdet(v, delta, x = None):
+    """
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    Currently returns two lists of tuples, but maybe arrays would be better
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    % [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    % maxima and minima ("peaks") in the vector V.
+    % MAXTAB and MINTAB consists of two columns. Column 1
+    % contains indices in V, and column 2 the found values.
+    %
+    % With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    % in MAXTAB and MINTAB are replaced with the corresponding
+    % X-values.
+    %
+    % A point is considered a maximum peak if it has the maximal
+    % value, and was preceded (to the left) by a value lower by
+    % DELTA.
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    """
+    maxtab = []
+    mintab = []
+       
+    if x is None:
+        x = arange(len(v))
+    
+    v = asarray(v)
+    
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+    
+    if not isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+    
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+    
+    mn, mx = Inf, -Inf
+    mnpos, mxpos = NaN, NaN
+    
+    lookformax = True
+    
+    for i in arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+ 
+    return array(maxtab), array(mintab)
 
 if __name__ == "__main__":
     print "Hello World";
