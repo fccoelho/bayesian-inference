394d8bad43dbd1307bcc86d17b9b219b0e358b64
Flavio Codeco Coelho <fccoelho@gmail.com>
2010-04-27 09:29:55 +0100

    added some more test skeletons

---
+++ trunk/BIP/Docs/source/overview.rst.before_restore_2010-04-21_14.39.58.289369
@@ -0,0 +1,8 @@
+Overview
+========
+
+The Bip Package is a collection of useful classes for basic Bayesian inference. Currently, its main goal is to be a tool for learning and exploration of Bayesian probabilistic calculations.
+
+Currently it also includes subpackages for stochastic simulation tools which are not strictly related to Bayesian inference, but are currently being developed within BIP. One such package is the BIP.SDE which contains a parallelized solver for stochastic differential equations, an implementation of the Gillespie direct algorithm.
+
+The Subpackage Bayes also offers a tool for parameter estimation of Deterministic and Stochastic Dynamical Models. This tool will be fully described briefly in a scientific paper currently submitted for publication. 
--- trunk/BIP/tests/test_Bayes_Samplers_MCMC.py
+++ trunk/BIP/tests/test_Bayes_Samplers_MCMC.py
@@ -26,3 +26,68 @@ class TestMetropolis:
         # assert_equal(expected, metropolis.tune(ar))
         raise SkipTest # TODO: implement your test here
 
+class test__Sampler:
+    def TestDIC(self):
+        # __sampler = _Sampler(parpriors, parnames)
+        # assert_equal(expected, __sampler.DIC())
+        raise SkipTest # TODO: implement your test here
+
+    def test___init__(self):
+        # __sampler = _Sampler(parpriors, parnames)
+        raise SkipTest # TODO: implement your test here
+
+    def test_dimensions(self):
+        # __sampler = _Sampler(parpriors, parnames)
+        # assert_equal(expected, __sampler.dimensions())
+        raise SkipTest # TODO: implement your test here
+
+    def test_gr_R(self):
+        # __sampler = _Sampler(parpriors, parnames)
+        # assert_equal(expected, __sampler.gr_R(end, start))
+        raise SkipTest # TODO: implement your test here
+
+    def test_gr_convergence(self):
+        # __sampler = _Sampler(parpriors, parnames)
+        # assert_equal(expected, __sampler.gr_convergence(relevantHistoryEnd, relevantHistoryStart))
+        raise SkipTest # TODO: implement your test here
+
+    def test_po(self):
+        # __sampler = _Sampler(parpriors, parnames)
+        # assert_equal(expected, __sampler.po())
+        raise SkipTest # TODO: implement your test here
+
+    def test_setup_xmlrpc_plotserver(self):
+        # __sampler = _Sampler(parpriors, parnames)
+        # assert_equal(expected, __sampler.setup_xmlrpc_plotserver())
+        raise SkipTest # TODO: implement your test here
+
+    def test_term_pool(self):
+        # __sampler = _Sampler(parpriors, parnames)
+        # assert_equal(expected, __sampler.term_pool())
+        raise SkipTest # TODO: implement your test here
+
+class TestModelAsRa:
+    def test_model_as_ra(self):
+        # assert_equal(expected, model_as_ra(theta, model, phinames))
+        raise SkipTest # TODO: implement your test here
+
+class TestDream:
+    def test___init__(self):
+        # dream = Dream(meldobj, samples, sampmax, data, t, parpriors, parnames, parlimits, likfun, likvariance, burnin, thin, convergenceCriteria, nCR, DEpairs, adaptationRate, eps, mConvergence, mAccept, **kwargs)
+        raise SkipTest # TODO: implement your test here
+
+    def test_delayed_rejection(self):
+        # dream = Dream(meldobj, samples, sampmax, data, t, parpriors, parnames, parlimits, likfun, likvariance, burnin, thin, convergenceCriteria, nCR, DEpairs, adaptationRate, eps, mConvergence, mAccept, **kwargs)
+        # assert_equal(expected, dream.delayed_rejection(xi, zi, pxi, zprob))
+        raise SkipTest # TODO: implement your test here
+
+    def test_step(self):
+        # dream = Dream(meldobj, samples, sampmax, data, t, parpriors, parnames, parlimits, likfun, likvariance, burnin, thin, convergenceCriteria, nCR, DEpairs, adaptationRate, eps, mConvergence, mAccept, **kwargs)
+        # assert_equal(expected, dream.step())
+        raise SkipTest # TODO: implement your test here
+
+    def test_update_CR_dist(self):
+        # dream = Dream(meldobj, samples, sampmax, data, t, parpriors, parnames, parlimits, likfun, likvariance, burnin, thin, convergenceCriteria, nCR, DEpairs, adaptationRate, eps, mConvergence, mAccept, **kwargs)
+        # assert_equal(expected, dream.update_CR_dist())
+        raise SkipTest # TODO: implement your test here
+
