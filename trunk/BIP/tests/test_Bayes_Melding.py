58a4a20d4ef5e6a3e9b3857fd5f8f5f2f70ed6c0
Flavio Codeco Coelho <fccoelho@gmail.com>
2010-04-28 11:16:44 +0100

    fixed bug in _rms_error() method

--- trunk/BIP/tests/test_Bayes_Melding.py
+++ trunk/BIP/tests/test_Bayes_Melding.py
@@ -1,40 +1,38 @@
-import unittest
 from BIP.Bayes import Melding
 from numpy.testing import *
-import scipy.stats as stfrom nose.tools import assert_equal
+import scipy.stats as st
+from nose.tools import assert_equal
 from nose import SkipTest
+from scipy.integrate import odeint
 
+K=50
+inits = [.999,0.001,0]
+tf = 25
+thetanames = ['beta','tau']
+phinames = ['S','I','R']
+verbose = False
 
-class TestFitModel(unittest.TestCase):
+def model(*theta):
+    try:#for standalone runs
+        inits = y0
+    except NameError:#for runing within a FitModel object
+        inits = self.inits
+    step = .1
+    beta,tau = theta
+    def sir(y,t):
+        '''ODE model'''
+        S,I,R = y
+        return  [-beta*I*S, #dS/dt
+                beta*I*S - tau*I, #dI/dt
+                tau*I, #dR/dt
+                ]
+    y = odeint(sir,inits,np.arange(t0,tf,step))
+    return y
+    
+class TestFitModel:
     def test___init__(self):
-        K=50
-        L=10
-        ntheta = 2
-        nphi = 3
-        inits = [.999,0.001,0]
-        tf = 25
-        thetanames = ['beta','tau']
-        phinames = ['S','I','R']
-        verbose = False
-        
-        def model(*theta):
-            try:#for standalone runs
-                inits = y0
-            except NameError:#for runing within a FitModel object
-                inits = self.inits
-            step = .1
-            beta,tau = theta
-            def sir(y,t):
-                '''ODE model'''
-                S,I,R = y
-                return  [-beta*I*S, #dS/dt
-                        beta*I*S - tau*I, #dI/dt
-                        tau*I, #dR/dt
-                        ]
-            y = odeint(sir,inits,np.arange(t0,tf,step))
-            return y
         self.model = model
-        self.fit_model = Melding.FitModel(K, L, model, ntheta, nphi, inits, tf, thetanames, phinames, verbose)
+        self.fit_model = Melding.FitModel(K, model, inits, tf, thetanames, phinames, verbose=verbose)
         assert isinstance(self.fit_model,Melding.FitModel)
 
     def test_do_inference(self):
@@ -99,7 +97,7 @@ class TestFitModel(unittest.TestCase):
         # assert_equal(expected, fit_model.prior_sample())
         raise SkipTest # TODO: implement your test here
 
-class TestMeld(unittest.TestCase):
+class TestMeld:
     def test___init__(self):
         self.meld = Meld(2000, 100, Melding.model, 2, 1, alpha=0.5, verbose=False, viz=False)
         assert isinstance(self.meld, Melding.Meld) 
@@ -107,72 +105,72 @@ class TestMeld(unittest.TestCase):
     def test_abcRun(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.abcRun(fitfun, data, t, nopool, savetemp))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_add_salt(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.add_salt(dataset, band))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_basicfit(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.basicfit(s1, s2))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_filtM(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.filtM(cond, x, limits))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_getPosteriors(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.getPosteriors(t))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_imp_sample(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.imp_sample(n, data, w))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_logPooling(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.logPooling(phi))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_mcmc_run(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.mcmc_run(data, t, likvariance, burnin, nopool, method))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_mh(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.mh(n, t, data, likfun, variance, burnin))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_run(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.run(*args))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_runModel(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.runModel(savetemp, t, k))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_setPhi(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.setPhi(names, dists, pars, limits))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_setPhiFromData(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.setPhiFromData(names, data, limits))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_setTheta(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
         # self.assertEqual(expected, meld.setTheta(names, dists, pars))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
     def test_setThetaFromData(self):
         # meld = Meld(K, L, model, ntheta, nphi, alpha, verbose, viz)
@@ -185,35 +183,34 @@ class TestMeld(unittest.TestCase):
         # self.assertEqual(expected, meld.sir(data, t, variance, nopool, savetemp))
         assert_almost_equal(pp.P.mean(),7.5,1)
 
-class TestEnumRun(unittest.TestCase):
+class TestEnumRun:
     def test_enum_run(self):
         # self.assertEqual(expected, enumRun(model, theta, k))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
-class TestModel(unittest.TestCase):
+class TestMode:
     def test_model(self):
         # self.assertEqual(expected, model(r, p0, n))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
-class TestPlotRaHist(unittest.TestCase):
+class TestPlotRaHist:
     def test_plot_ra_hist(self):
         # self.assertEqual(expected, plotRaHist(arr))
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
-class TestMain2(unittest.TestCase):
+class TestMain2:
     def test_main2(self):
         # self.assertEqual(expected, main2())
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
-class TestMhTest(unittest.TestCase):
+class TestMhTes:
     def test_mh_test(self):
         # self.assertEqual(expected, mh_test())
-        assert False # TODO: implement your test here
+        raise SkipTest # TODO: implement your test here
 
 class TestBasicfit:
     def test_basicfit(self):
         # assert_equal(expected, basicfit(s1, s2))
         raise SkipTest # TODO: implement your test here
 
-if __name__ == '__main__':
-    unittest.main()
+
