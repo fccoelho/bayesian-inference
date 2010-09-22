40c4525d983210ed6e8f9dafa26282fb523e30af
Flavio Codeco Coelho <fccoelho@gmail.com>
2010-09-22 13:31:24 -0300

    added try except clause to handle oserror when joining processes

--- trunk/BIP/Bayes/Melding.py
+++ trunk/BIP/Bayes/Melding.py
@@ -700,6 +700,7 @@ class Meld(object):
         self.BIC = None
         self.DIC = None
         self.stop_now = False #Flag to interrupt fitting loop. inner loop checks this flag and exit when it is true.
+        self.current_step = 0
         self.proposal_variance = 0.0000001
         self.adaptscalefactor = 1 #adaptive variance. Used my metropolis hastings
         self.salt_band = 0.1 #relaxation factor of the prior limits
