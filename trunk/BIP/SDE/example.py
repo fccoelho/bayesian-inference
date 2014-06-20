21c6ff4bf1508bdbcb8d834714db274568d6b6d8
fccoelho <fccoelho@flavio-ultra>
2014-06-20 12:25:36 -0300

    added a few optimizations to cgillespie; fixed setup.py so that it correctly builds cython modules; added cython_GSL to requirements

--- trunk/BIP/SDE/example.py
+++ trunk/BIP/SDE/example.py
@@ -41,8 +41,10 @@ Ic -> R : rc*Ic
 # pyximport.install()
 from gillespie import Model
 import time
+import cProfile, pstats
 from numpy import array
 from cgillespie import Model as CModel
+
 
 vnames = ['S','E','Is','Ic','R']
 #rates: b,ks,kc,rs,rc
@@ -69,29 +71,32 @@ CM = CModel(vnames=vnames, rates=r, inits=ini, tmat=tmat, propensity=propf)
 
 # timing python gillespie
 t0 = time.time()
-M.run(tmax=80, reps=1000, viz=0, serial=0)
+#M.run(tmax=80, reps=1000, viz=0, serial=1)
 pt = time.time()-t0
-print 'Python total time: ',pt, ' seconds.'
-t, series, steps, evts = M.getStats()
+print 'Python total time: ', pt, ' seconds.'
+#t, series, steps, evts = M.getStats()
 #print evts
-print steps,'steps'
+#print steps,'steps'
 
 # timing cython gillespie
+cProfile.runctx("CM.run(tmax=80, reps=1000)",globals(), locals(), 'cgil_Profile.prof')
 t0 = time.time()
-CM.run(tmax=80, reps=1000)
+#CM.run(tmax=80, reps=1000)
 ct = time.time()-t0
 print 'Cython total time: ',ct, ' seconds.'
-t2, series2, steps2 = CM.getStats()
-print steps2, ' steps'
-print "Cython speedup: %sx" % (pt/ct)
-from pylab import plot, show, legend, errorbar, title, figure
-print series.shape
-#print cevts
-plot(t, series.mean(axis=0),'-o')
-title('Python curve')
-legend(vnames, loc=0)
-figure()
-plot(t2, series2.mean(axis=2), '-o')
-title('Cython curve')
-legend(vnames, loc=0)
-show()
+s = pstats.Stats('cgil_Profile.prof')
+s.strip_dirs().sort_stats("time").print_stats()
+#t2, series2, steps2 = CM.getStats()
+#print steps2, ' steps'
+# print "Cython speedup: %sx" % (pt/ct)
+# from pylab import plot, show, legend, errorbar, title, figure
+# print series.shape
+# #print cevts
+# plot(t, series.mean(axis=0),'-o')
+# title('Python curve')
+# legend(vnames, loc=0)
+# figure()
+# plot(t2, series2.mean(axis=2), '-o')
+# title('Cython curve')
+# legend(vnames, loc=0)
+# show()
--- trunk/setup.py
+++ trunk/setup.py
@@ -1,8 +1,12 @@
 # -*- coding:utf-8 -*-
 
-from distutils.core import setup
-from Cython.Build import cythonize
+from setuptools import setup
 from setuptools import find_packages
+from Cython.Build import cythonize
+from Cython.Distutils import Extension
+from Cython.Distutils import build_ext
+import cython_gsl
+
 
 
 setup(name='BIP',
@@ -15,11 +19,15 @@ setup(name='BIP',
       packages=find_packages(),
       #['','BIP','BIP.SDE','BIP.Bayes','BIP.SMC','BIP.Bayes.general','BIP.Bayes.conjugate','BIP.Bayes.tests','BIP.Viz'],
       package_data={'': ['*.txt']},
-      # ext_modules=cythonize("BIP/SDE/cgillespie.pyx"),
-      install_requires=["numpy", "scipy", "openopt", "liveplots", "cython", "gnuplot-py"],
+      include_dirs=[cython_gsl.get_include()],
+      ext_modules=cythonize([Extension("cgillespie", sources=["BIP/SDE/cgillespie.pyx"],
+                             libraries=cython_gsl.get_libraries(),
+                             library_dirs=[cython_gsl.get_library_dir()],
+                             include_dirs=[cython_gsl.get_cython_include_dir()])]),
+      install_requires=["numpy", "scipy", "openopt", "liveplots", "cython", "gnuplot-py", "cython_gsl"],
       test_suite='nose.collector',
       license='GPLv3',
-      #        cmdclass = {'build_ext': build_ext},
+      cmdclass={'build_ext': build_ext},
       #ext_modules=[Extension('BIP/SDE/gillespie', ['BIP/SDE/gillespie.c'])],
 
 )
