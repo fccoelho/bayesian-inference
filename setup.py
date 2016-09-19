1e422cc26a9c7934a3a5a29502ded2fd5f3241de
Flávio Codeço Coelho <fccoelho@gmail.com>
2016-09-19 09:31:30 -0300

    Fixed bug in the setup.py which did not package *.pyx files

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 from __future__ import print_function
-# -*- coding:utf-8 -*-
 
 from setuptools import setup, Extension
 from setuptools import find_packages
@@ -8,12 +7,11 @@ from Cython.Build import cythonize
 from Cython.Distutils import Extension
 from Cython.Distutils import build_ext
 
-
 extensions = [Extension('BIP.SDE.cgillespie', ["BIP/SDE/cgillespie.pyx"]), ]
 print(cythonize(extensions))
 
 setup(name='BIP',
-      version='0.6.4',
+      version='0.6.6',
       author='Flavio Codeco Coelho',
       author_email='fccoelho@gmail.com',
       url='http://code.google.com/p/bayesian-inference/',
@@ -23,15 +21,16 @@ setup(name='BIP',
       # ['','BIP','BIP.SDE','BIP.Bayes','BIP.SMC','BIP.Bayes.general','BIP.Bayes.conjugate','BIP.Bayes.tests','BIP.Viz'],
       package_data={'': ['*.txt']},
       ext_modules=cythonize(extensions),
+      setup_requires=["cython", "numpy", "cythongsl"],
       install_requires=["numpy", "scipy", "openopt", "liveplots", "cython"],
       test_suite='nose.collector',
       license='GPLv3',
       cmdclass={'build_ext': build_ext},
-      #ext_modules=[Extension('BIP/SDE/gillespie', ['BIP/SDE/gillespie.c'])],
-
+      # ext_modules=[Extension('BIP/SDE/gillespie', ['BIP/SDE/gillespie.c'])],
       )
 
 import os
+
 # This to avoid creating the log file with super-user privileges during instalation.
 try:
     os.unlink('/tmp/BIP.log')
